9268d64aeab83d9552f8aee69f12f30a34fbb0c7
Krachunov <Krachunov@Krachunov-PC>
2015-08-17 17:18:49 +0300

    Problem 1 solved

--- HomeWork7/src/Problem1.java
+++ HomeWork7/src/Problem1.java
@@ -4,21 +4,62 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Problem1 {
+	private Map<String, Integer> dictionary;
 
-	public static List<String> readFile(File inputFile) throws FileNotFoundException,
-			IOException {
-		List<String> everyLine = new ArrayList<String>();
+	/**
+	 * 
+	 * @param inputFile
+	 *            - input file
+	 * @return List with single word in to line
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	private static List<String> readLineFromFile(File inputFile)
+			throws FileNotFoundException, IOException {
+		List<String> everyWord = new ArrayList<String>();
+
 		try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
 			String line = br.readLine();
 			while (line != null) {
-				everyLine.add(line);
+				String[] words = line.split(" ");
+				for (String word : words) {
+					everyWord.add(word.trim());
+				}
 				line = br.readLine();
 			}
 		}
-		return everyLine;
-
+		return everyWord;
 	}
+
+	public void FindWordInFile(String fileName) throws FileNotFoundException,
+			IOException {
+		List<String> text = readLineFromFile(new File(fileName));
+		Map<String, Integer> dictionary = new HashMap();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of word");
+		int numberWord = sc.nextInt();
+		System.out.println("Enter " + numberWord + " words:");
+		for (int i = 0; i < numberWord; i++) {
+			String word = sc.next();
+			dictionary.put(word, 0);
+		}
+		int currentCount = -1;
+		for (String word : text) {
+			if (dictionary.containsKey(word)) {
+				currentCount = dictionary.get(word);
+				dictionary.put(word, currentCount + 1);
+			}
+		}
+		for (Map.Entry<String, Integer> entry : dictionary.entrySet()) {
+			System.out.println(entry.getKey() + " -> " + entry.getValue());
+		}
+	}
+
 }
--- HomeWork7/src/Problem1Test.java
+++ HomeWork7/src/Problem1Test.java
@@ -7,10 +7,10 @@ public class Problem1Test {
 
 	public static void main(String[] args) throws FileNotFoundException,
 			IOException {
-		File file = new File("D:\\test.txt");
-
-		List<String> list = Problem1.readFile(file);
-		System.out.println(list.size());
+		Problem1 test = new Problem1();
+		
+		test.FindWordInFile("d:\\test.txt");
+		
 	}
 
 }
