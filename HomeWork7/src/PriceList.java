de4359380134d29fb3fa8aa4f78850bbd05fad3d
Krachunov <Krachunov@Krachunov-PC>
2015-08-18 17:29:21 +0300

    Equals method to product class

---
+++ HomeWork7/src/PriceList.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeMap;
+
+/**
+ * Problem 2
+ */
+public class PriceList {
+	private TreeMap<Integer, List<Product>> product;
+	private int count;
+
+	public TreeMap<Integer, List<Product>> getProduct() {
+		return product;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public PriceList() {
+		product = new TreeMap<Integer, List<Product>>();
+		setCount(0);
+	}
+
+	public boolean add(String value, int price) {
+		if (!getProduct().containsKey(price)) {
+			List<Product> currentList = new ArrayList<Product>();
+			currentList.add(new Product(value, price));
+			getProduct().put(price, currentList);
+			int currentCount = getCount();
+			setCount(currentCount + 1);
+			return true;
+		} else {
+			List<Product> currentList = getProduct().get(price);
+			Product newProduct = new Product(value, price);
+			if (!currentList.contains(newProduct)) {
+				// TODO - do not work
+				currentList.add(newProduct);
+				int currentCount = getCount();
+				setCount(currentCount + 1);
+				return true;
+			} else {
+				System.out.println("Already in list");
+				return false;
+			}
+		}
+	}
+
+	private class Product {
+		private String value;
+		private int price;
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+		public Product(String value, int price) {
+			setValue(value);
+			setPrice(price);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this.getValue().equals(((Product) o).getValue())) {
+				return this.getPrice() == ((Product) o).getPrice();
+			}
+			return false;
+
+		}
+	}
+
+}
---
+++ HomeWork7/src/PriceListTest.java
@@ -0,0 +1,11 @@
+public class PriceListTest {
+
+	public static void main(String[] args) {
+		PriceList priceList = new PriceList();
+
+		priceList.add("drugo", 130);
+		priceList.add("drugo", 130);
+
+	}
+
+}
