015511ce3f4c7668c6aee1743df26dafa89f1428
Krachunov <Krachunov@Krachunov-PC>
2015-08-21 18:19:10 +0300

    Version 2

--- HomeWork7/src/MyBinaryHeapTest.java
+++ HomeWork7/src/MyBinaryHeapTest.java
@@ -1,22 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class MyBinaryHeapTest {
 
 	public static void main(String[] args) {
 		MyBinaryHeaps2<Integer> myHeap = new MyBinaryHeaps2<Integer>();
 
-		myHeap.Enqueue(20);
-		myHeap.Enqueue(8);
-		myHeap.Enqueue(30);
-		myHeap.Enqueue(5);
-		myHeap.Enqueue(50);
+		myHeap.enqueue(20);
+		myHeap.enqueue(8);
+		myHeap.enqueue(30);
+		myHeap.enqueue(5);
+		myHeap.enqueue(50);
+
+		myHeap.print();
+
+//		System.out.println(myHeap.dequeue());
+//		System.out.println(myHeap.dequeue());
 	
-		for (Object element : myHeap.getArr()) {
-			System.out.println(element);
+
+		PriorityQueue<Integer> a = new PriorityQueue<Integer>();
+
+		a.add(20);
+		a.add(8);
+		a.add(30);
+		a.add(5);
+		a.add(50);
+		System.out.println("------");
+		for (Integer integer : a) {
+			System.out.println(integer);
 		}
 
-	
-		
-	
-	}
 
+	}
 }
--- HomeWork7/src/MyBinaryHeaps2.java
+++ HomeWork7/src/MyBinaryHeaps2.java
@@ -10,7 +10,7 @@ public class MyBinaryHeaps2<T extends Comparable<T>> implements Comparator<T> {
 	private Object[] arr;
 	private T element;
 	private final int SIZE = 16;
-	private int count = 1;
+	private int count = 0;
 
 	public T getElement() {
 		return element;
@@ -40,11 +40,38 @@ public class MyBinaryHeaps2<T extends Comparable<T>> implements Comparator<T> {
 	/**
 	 * Resize double the array
 	 */
-	public void grow() {
+	private void grow() {
 		if (count == getArr().length) {
 			Object[] newArr = new Object[arr.length * 2];
 			System.arraycopy(arr, 0, newArr, 0, arr.length);
 			this.arr = newArr;
+		}
+	}
+
+	private int leftChild(int index) {
+		return (index * 2 < getArr().length - 1 ? index * 2 : -1);
+	}
+
+	private int rightChild(int index) {
+		return ((index * 2 + 1) < getArr().length - 1 ? (index * 2 + 1) : -1);
+	}
+
+	private int parentIndex(int index) {
+		return (index / 2 > 0 ? index / 2 : index / 2 == 0 ? 0 : -1);
+	}
+
+	private int smalestChild(int left, int right) {
+		T leftChild = (T) getArr()[left];
+		T rightChild = (T) getArr()[right];
+
+		if (left > 0 && right > 0 && leftChild.compareTo(rightChild) > 0) {
+			return right;
+		} else if (left > 0 && right > 0 && leftChild.compareTo(rightChild) < 0) {
+			return left;
+		} else if (left > 0 && right < 0) {
+			return left;
+		} else {
+			return right;
 		}
 	}
 
@@ -55,61 +82,96 @@ public class MyBinaryHeaps2<T extends Comparable<T>> implements Comparator<T> {
 	 * @param element2
 	 *            - parent to new Element
 	 */
-	public void swap(T child, T parent, int indexToStartSwap) {
-		int currentIndex = indexToStartSwap;
-		int parrentIndex = currentIndex / 2;
-		getArr()[parrentIndex] = child;
-		getArr()[currentIndex] = parent;
+	private void swap(int element1, int element2) {
+		T firstElement = (T) getArr()[element1];
+		T secondElement = (T) getArr()[element2];
+		getArr()[element1] = secondElement;
+		getArr()[element2] = firstElement;
 	}
 
 	/**
-	 * Relocate two element - children and parent.
+	 * Relocate two element - children and parent. Swap them
 	 */
 	@SuppressWarnings("unchecked")
-	public void relocateNewNode() {
-		int currentIdex = getCount();
-		int parentIndex = currentIdex / 2;
-		T child = (T) getArr()[currentIdex];
+	private void relocateNewNode() {
+		// get the last add element
+		int currentIndex = getCount();
+		int parentIndex = parentIndex(currentIndex);
+		T child = (T) getArr()[currentIndex];
 		T parent = (T) getArr()[parentIndex];
 		// TODO
-		while (child.compareTo(parent) > 0 && currentIdex > 1) {
-			swap(child, parent, currentIdex);
-			currentIdex = getCount() / 2;
-			if (currentIdex < 2) {
+		while (parent.compareTo(child) > 0 && currentIndex > 0) {
+			swap(currentIndex, parentIndex);
+			currentIndex = parentIndex / 2;
+			if (currentIndex < 1) {
 				break;
 			}
-			parentIndex = currentIdex / 2;
-			child = (T) getArr()[currentIdex];
+			parentIndex = parentIndex(currentIndex);
+			child = (T) getArr()[currentIndex];
 			parent = (T) getArr()[parentIndex];
 		}
-
 	}
-/**
- * Add new element
- * @param element - new Element
- * @return - true or false
- */
-	public boolean Enqueue(T element) {
+
+	/**
+	 * Add new element
+	 * 
+	 * @param element
+	 *            - new Element
+	 * @return - true or false
+	 */
+	public boolean enqueue(T element) {
 		grow();
 		int index = getCount();
 		getArr()[index] = element;
-		if (getCount() > 2) {
+		if (getCount() >= 2) {
 			relocateNewNode();
 		}
 		setCount(getCount() + 1);
-
 		return true;
 	}
 
-	public boolean remove(T element) {
-		// TODO
-		return false;
-	}
+	@SuppressWarnings("unchecked")
+	public T dequeue() {
+		if (getCount() == 0) {
+			throw new IllegalArgumentException("The queue is empty");
+		}
+		T element = (T) getArr()[0];
+		T newEl = (T) getArr()[getCount() - 1];
+		getArr()[1] = newEl;
+		getArr()[getCount() - 1] = null;
+		setCount(getCount() - 1);
 
-	public T find(T element) {
-		// TODO
+		int currentIndex = 0;
+		int leftChild = leftChild(currentIndex);
+		int rightChild = rightChild(currentIndex);
+		int smallestChild = smalestChild(leftChild, rightChild);
+
+		T parent = (T) getArr()[currentIndex];
+		T childToChange = (T) getArr()[smallestChild];
+
+		while (smallestChild > 0 && parent.compareTo(childToChange) > 0) {
+			swap(currentIndex, smallestChild);
+			currentIndex = smallestChild;
+			leftChild = leftChild(currentIndex);
+			rightChild = rightChild(currentIndex);
+			smallestChild = smalestChild(leftChild, rightChild);
+			parent = (T) getArr()[currentIndex];
+			childToChange = (T) getArr()[smallestChild];
+		}
+
 		return element;
-
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		return (this == obj);
+	}
+
+	public void print() {
+		for (Object element : this.getArr()) {
+			if (element != null) {
+				System.out.println(element);
+			}
+		}
+	}
 }
